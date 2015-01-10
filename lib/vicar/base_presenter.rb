4ba6eb573d096066c0b79ed1332997060e681422
Меркушин Михаил <bibendi@bk.ru>
2015-01-10 18:08:11 +0500

    Update base_presenter.rb

--- lib/vicar/base_presenter.rb
+++ lib/vicar/base_presenter.rb
@@ -2,5 +2,20 @@ require 'delegate'
 
 module Vicar
   class BasePresenter < SimpleDelegator
+    def complex_meth
+      if 1 == 2 && 2 == 1 && 3 == 4 && 4 == 7
+        puts 1
+      end
+      
+      if 1 == 2 && 2 == 1 && 3 == 4 && 4 == 7
+        puts 1
+      end
+      if 1 == 2 && 2 == 1 && 3 == 4 && 4 == 7
+        puts 1
+      end
+      if 1 == 2 && 2 == 1 && 3 == 4 && 4 == 7
+        puts 1
+      end
+    end
   end
 end
