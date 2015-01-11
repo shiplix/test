7c4ca71827f5f05fb02b3e71aebf452c7a774562
Меркушин Михаил <bibendi@bk.ru>
2015-01-11 19:20:20 +0500

    Update rails_extensions.rb

--- lib/vicar/rails_extensions.rb
+++ lib/vicar/rails_extensions.rb
@@ -3,13 +3,17 @@ module Vicar
     autoload :ActionView, 'vicar/rails_extensions/action_view'
     
     def dubl1
-      if Rails.env.development
+      if Rails.env.development? && 1 == 2 && 2 == 3
+        b = 2
         call_method(1, :test, true)
+      end
     end
     
      def dubl2
-      if Rails.env.development
+      if Rails.env.development? && 1 == 2 && 2 == 3
+        b = 2
         call_method(1, :test, true)
+      end
     end
   end
 end
