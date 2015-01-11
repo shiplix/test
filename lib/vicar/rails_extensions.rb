a84960bacdf21846e316838e183f920b86179620
Меркушин Михаил <bibendi@bk.ru>
2015-01-11 17:51:54 +0500

    Update rails_extensions.rb

--- lib/vicar/rails_extensions.rb
+++ lib/vicar/rails_extensions.rb
@@ -1,5 +1,15 @@
 module Vicar
   module RailsExtensions
     autoload :ActionView, 'vicar/rails_extensions/action_view'
+    
+    def dubl1
+      if Rails.env.development
+        call_method(1, :test, true)
+    end
+    
+     def dubl2
+      if Rails.env.development
+        call_method(1, :test, true)
+    end
   end
 end
