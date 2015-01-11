91cada94cba80d8fa52d76dc4210c368cd9743c4
Меркушин Михаил <bibendi@bk.ru>
2015-01-11 19:28:18 +0500

    Create test.rb

---
+++ lib/vicar/test.rb
@@ -0,0 +1,21 @@
+class A
+  def meth
+    puts 1 if 1 == 0
+  end
+end
+
+class B
+  def dabl
+    if ff == hh && nn == mm
+      cc = 101
+      puts 'qwerty'
+    end
+  end
+  
+  def dab2
+    if ff == hh && nn == mm
+      cc = 101
+      puts 'qwerty'
+    end
+  end
+end
