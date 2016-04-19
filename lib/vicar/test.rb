61b14ee68f029c1c81678e57cedec8c8f7113e16
Michail Merkushin <bibendi@bk.ru>
2016-04-19 18:10:49 +0500

    Update test.rb

--- lib/vicar/test.rb
+++ lib/vicar/test.rb
@@ -27,3 +27,21 @@ class B
     end
   end
 end
+
+module Flay
+  class DirtyClass
+    def x
+      if params[:name] && params[:name] != ''
+        conditions << '(lower(name) like :name)'
+        placeholders[:name] = '%' + params[:name].downcase + '%'
+      end
+    end
+
+    def y
+      if params[:name] && params[:name] != ''
+        conditions << '(lower(name) like :name)'
+        placeholders[:name] = '%' + params[:name].downcase + '%'
+      end
+    end
+  end
+end
